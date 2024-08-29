5241c908c22110add73b205d11103307fcd4bf89
Cesar Hernandez <149346749+cesarurielhr@users.noreply.github.com>
2024-08-28 23:24:11 -0700

    create ej4.js

---
+++ ej4.js
@@ -0,0 +1,10 @@
+let numeros = [115, 8, 12, 1, 5, 54];
+
+// Ordenar el arreglo de menor a mayor
+numeros.sort(function (a, b) {
+  return a - b;
+});
+
+
+// Mostrar el arreglo 
+console.log('Arreglo ordenado: ' + numeros);
